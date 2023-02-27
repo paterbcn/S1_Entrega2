4ae95611a2994f147af8589a5d02162c4ae9a50f
pater <agustinpaternoster@gmail.com>
2023-02-27 15:12:10 +0100

    "correcciones finales"

--- N2-Ejercicio1.js
+++ N2-Ejercicio1.js
@@ -1,9 +1,9 @@
-/*Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.*/
+/*Crea una arrow function que, rebent un paràmetre, retorni un objecte
+ amb un atribut que tingui com a valor el paràmetre rebut.*/
 
-const persona = nom => {
-    const Datos ={ nombre :nom , nick: nom + nom.lenght}
-    return Datos}
+const persona = (nom => {
+    const Datos ={ nombre :nom , nick: nom.replace(" ","").toLowerCase() + nom.length}
+    return Datos})("juan Garcia")
 
 
-console.log(persona.nombre)
 
